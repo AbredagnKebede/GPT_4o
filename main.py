a4e88f98d83ddf3e7f8afc72679b292f9c6d56f6
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-10-28 22:16:09 +0300

    new add

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import os
 import json
 import streamlit as st
 import openai
-
 #configuration 
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config = json.load(open(f"{working_dir}/config.json"))
