78a78921a1f4f6bc6d09d19bc70613da58ea33c6
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-11-02 12:00:19 +0300

    come

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#GPT 3.5tobo open ai simulation
+#GPT 3.5 openai simulation
 import os
 import json
 import streamlit as st
@@ -9,10 +9,11 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 config_data = json.load(open(f"{working_dir}/config.json"))
 OPENAI_API_KEY = config_data["OPENAI_API_KEY"]
 openai.api_key = OPENAI_API_KEY
+
 #configuring streamlit page setting 
 st.set_page_config(page_title = "GPT 4 ASTU",page_icon = "💬", layout = "centered")
 
-#initializing chat session
+#initializing chat session that is a list that append the cha t history
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
@@ -23,5 +24,12 @@ for message in st.session_state.chat_history:
         st.markdown(message['content'])
 user_prompt = st.chat_input("Ask GPT ASTU... ")
 if user_prompt:
-     st.session_state.chat_history.append({'role':'user', 'content':user_prompt})
-     
+    st.chat_message.markdown(user_prompt)
+    st.session_state.chat_history.append({'role': 'user', 'content': user_prompt})
+    
+respons = openai.chat.copletions.create(
+    model = "gpt 3.5",
+    message = [
+        {'role': 'system', 'content': "you are my helpful assistant"}
+    ]
+)
