c8d19fcd23c0998b681bb96f53d11a7c002f6aad
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-11-04 00:11:21 +0300

    modification

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ OPENAI_API_KEY = config_data["OPENAI_API_KEY"]
 openai.api_key = OPENAI_API_KEY
 
 #configuring streamlit page setting 
-st.set_page_config(page_title = "GPT 4 ASTU",page_icon = "💬", layout = "centered")
+st.set_page_config(
+    page_title = "GPT 4 ASTU",
+    page_icon = "💬",
+    layout = "centered")
 
-#initializing chat session that is a list that append the cha t history
+#initializing chat session that is a list appends the chat history
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
@@ -22,24 +25,29 @@ st.title("🤖 GPT 4 ASTU")
 for message in st.session_state.chat_history:
     with st.chat_history(message['role']):
         st.markdown(message['content'])
+
+#input field for users    
 user_prompt = st.chat_input("Ask GPT ASTU... ")
+
 if user_prompt:
-    st.chat_message.markdown(user_prompt)
+    #add user message and display it
+    st.chat_message('user').markdown(user_prompt)
     st.session_state.chat_history.append({'role': 'user', 'content': user_prompt})
     
-respons = openai.chat.completions.create(
-    model = "gpt 3.5",
-    message = [
-        {'role': 'system', 'content': "you are my helpful assistant"},
-        *st.session_state.chat_history
-    ]
-)
-#storing the model's response or answer 
-assistant_respons = respons.choice[0].message.content
+    response = openai.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {'role': 'system', 'content': "You are my helpful assistant"},
+            *st.session_state.chat_history
+        ]
+    )
+    
+    #storing the model's response or answer 
+    assistant_response = response.choices[0].message.content
 
-#appending the the respons to our model data
-st.session_state.chat_history.append({'role':'assistant','content':assistant_respons})
+    #appending the the respons to our model data
+    st.session_state.chat_history.append({'role':'assistant','content':assistant_response})
 
-#displaying the assistant respons
-with st.chat_message('assistant'):
-    st.markdown(assistant_respons)
+    #displaying the assistant respons
+    with st.chat_message('assistant'):
+        st.markdown(assistant_response)
