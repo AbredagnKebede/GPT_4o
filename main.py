acc2ec1e486fdadb1229c47f50e14a9e43b00cf7
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-11-10 13:41:57 +0300

    last update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#GPT 3.5 openai simulation
+#--- gpt-4o-mini openai simulation ---
 import os
 import json
 import streamlit as st
@@ -28,6 +28,7 @@ for message in st.session_state.chat_history:
 
 #input field for users    
 user_prompt = st.chat_input("Ask GPT ASTU... ")
+page_intro = "This is a simulation of chat gpt 4 using pretrained OPENAI mode provided by openai. Model simulation was built by Abredagn Kebede, thrid year software student as ASTU"
 
 if user_prompt:
     #add user message and display it
@@ -35,7 +36,7 @@ if user_prompt:
     st.session_state.chat_history.append({'role': 'user', 'content': user_prompt})
     
     response = openai.chat.completions.create(
-        model = "gpt-3.5-turbo",
+        model = "gpt-4o-mini",
         messages = [
             {'role': 'system', 'content': "You are my helpful assistant"},
             *st.session_state.chat_history
@@ -51,3 +52,5 @@ if user_prompt:
     #displaying the assistant respons
     with st.chat_message('assistant'):
         st.markdown(assistant_response)
+else:
+    print(page_intro)
